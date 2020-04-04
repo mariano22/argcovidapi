1e39a1d5ac3eb422d8b24cbd87230042c2e8734b
Mariano Crosetti <marianocrosetti1993@gmail.com>
2020-04-04 14:10:06 -0300

    Refactory

--- common.py
+++ common.py
@@ -11,6 +11,14 @@ def normalize_str(s):
     """ Function for name normalization (handle áéíóú). """
     return unicodedata.normalize("NFKD", s).encode("ascii","ignore").decode("ascii").upper()
 
+def download_csv(url, csv_output_file):
+    response = requests.get(url)
+    assert response.status_code == 200,\
+        'Wrong status code at dowloading from drive {}'.format(csv_output_file)
+    f = open(csv_output_file, "wb")
+    f.write(response.content)
+    f.close()
+
 def download_google_tables(drive_key, drive_uid_sheets):
     """ Download sheets from drive_key file. drive_uid_sheets must be
         [(drive_uid, output_filename)] """
@@ -18,9 +26,4 @@ def download_google_tables(drive_key, drive_uid_sheets):
     URL_TEMPLATE = 'https://docs.google.com/spreadsheet/ccc?key={}&output=csv&gid={}'
 
     for csv_filename, uid in drive_uid_sheets:
-        response = requests.get(URL_TEMPLATE.format(drive_key, uid))
-        assert response.status_code == 200,\
-            'Wrong status code at dowloading from drive {}'.format(csv_filename)
-        f = open(csv_filename, "wb")
-        f.write(response.content)
-        f.close()
+        download_csv(URL_TEMPLATE.format(drive_key, uid), csv_filename)
