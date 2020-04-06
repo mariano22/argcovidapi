ec374c94d56df46181ef55c0b689e15f1e7b2031
Mariano Crosetti <marianocrosetti1993@gmail.com>
2020-04-06 15:09:52 -0300

    datavizs

--- dataviz/dataviz_generator.py
+++ dataviz/dataviz_generator.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     with open('dataviz.md', 'w') as f:
         for fp in glob.glob('./*.png'):
             fn = os.path.basename(fp)
-            f.write('![{}]({})\n'.format(fn[:-4],fp))
+            f.write('![{}]("{}")\n'.format(fn[:-4],fp))
