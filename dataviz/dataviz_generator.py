d65c0d456028f573e274b8f11d716a51afa4fa02
Mariano Crosetti <marianocrosetti1993@gmail.com>
2020-04-09 21:38:50 -0300

    Erase dataviz from this repo
    It will be included in other repo.

--- dataviz/dataviz_generator.py
+++
@@ -1,8 +0,0 @@
-import os
-import glob
-
-if __name__ == '__main__':
-    with open('dataviz.md', 'w') as f:
-        for fp in glob.glob('./*.png'):
-            fn = os.path.basename(fp)
-            f.write('![{}]("{}")\n'.format(fn[:-4],fp))
