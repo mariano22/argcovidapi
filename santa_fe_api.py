5b0544cf98cb838d87a33c037cde821ed22544bb
Mariano Crosetti <marianocrosetti1993@gmail.com>
2020-04-29 17:13:13 -0300

    Add name sanity (colide city name synonyms) and update csvs

--- santa_fe_api.py
+++ santa_fe_api.py
@@ -29,17 +29,48 @@ def complete_deparments_and_total(df):
     df = df.copy()
     # Only cities on df
     df_cities = df[df.index.map(lambda x : is_city(x[2]))].copy()
-    # Group by department
-    df_departments = df_cities.groupby(['TYPE','DEPARTMENT']).sum()
-    # Group all cities
-    df_totals = df_cities.groupby(['TYPE']).sum()
-    # Iterate original dataframe replacing 'TOTAL' and departments rows
-    for index,_ in df.iterrows():
-        type_case, department, place_name = index[TYPE_INDEX], index[DEPARTMENT_INDEX], index[PLACE_INDEX]
-        if place_name=='##TOTAL':
-            df.loc[type_case].loc[department].loc[place_name] = df_totals.loc[type_case]
-        elif is_deparment(place_name):
-            df.loc[type_case].loc[department].loc[place_name] = df_departments.loc[type_case].loc[department]
+    # Group by department and shape similar to df
+    df_departments = df_cities.groupby(['TYPE','DEPARTMENT']).sum().reset_index()
+    df_departments['PLACE']=df_departments['DEPARTMENT']
+    df_departments = df_departments.set_index(['TYPE','DEPARTMENT','PLACE'])
+    # Group all cities and shape similar to df
+    df_totals = df_cities.groupby(['TYPE']).sum().reset_index()
+    df_totals['PLACE']='##TOTAL'
+    df_totals['DEPARTMENT']='##TOTAL'
+    df_totals = df_totals.set_index(['TYPE','DEPARTMENT','PLACE'])
+    # Filter 'TOTAL' and departments rows and add the one that calculated
+    df = df[ df.index.map(lambda i : '#' not in i[PLACE_INDEX]) ]
+    df = pd.concat([df,df_departments,df_totals]).sort_index()
+    return df
+
+def _colide_names(df, name_predicate, new_name):
+    is_named = df['PLACE'].apply(name_predicate)
+    df_match = df[is_named].copy()
+    df_match['PLACE']=normalize_str(new_name)
+    df_match = df_match.groupby(['TYPE','DEPARTMENT','PLACE']).sum()
+
+    df = pd.concat([ df[~is_named].copy().set_index(['TYPE','DEPARTMENT','PLACE']), df_match])
+    df = df.reset_index()
+    return df
+
+def name_sanity(df):
+    df = df.reset_index()
+    df['PLACE'] = df['PLACE'].replace({
+        'PUEBLO ANDINO':'ANDINO',
+        'BERAVEBU':'BERABEVU',
+        'SANTA ROSA DE CALCHINES':'SANTA ROSA',
+        'SAN JERONIMO SUD':'SAN JERONIMO SUR',
+        'PUEBLO IRIGOYEN':'IRIGOYEN',
+        'DIAZ':'ESTACION DIAZ',
+        'VILLA GOB. GALVEZ': 'VILLA GOBERNADOR GALVEZ',
+        'CLASSON':'CLASON',
+        'CAFFERATA':'CAFERATA',
+    })
+
+    df=_colide_names(df,lambda p : 'DOMINGUEZ' in p or 'CORONEL RODOLFO' in p ,'Coronel Dominguez')
+    df=_colide_names(df,lambda p : 'PUERTO' in p and 'SAN MARTIN' in p ,'Puerto San Martin')
+    df=_colide_names(df,lambda p : 'VILLA' in p and 'GALVEZ' in p ,'VILLA GOBERNADOR GALVEZ')
+    df=df.set_index(['TYPE','DEPARTMENT','PLACE']).sort_index()
     return df
 
 class SantaFeAPI:
@@ -61,6 +92,9 @@ class SantaFeAPI:
         self.df = self.df.reorder_levels(['TYPE','DEPARTMENT','PLACE'])
         TYPE_INDEX, DEPARTMENT_INDEX, PLACE_INDEX = 0, 1, 2
         self.df = self.df.sort_index()
+
+        # Rename some columns, colide synonyms cities
+        self.df = name_sanity(self.df)
 
         # Get cities name
         self.all_names = set(index[PLACE_INDEX] for index in self.df.index)
