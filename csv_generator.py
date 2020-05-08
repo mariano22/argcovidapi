768edab2f6e9465cccc6c37ced005bb708afd7de
Mariano Crosetti <marianocrosetti1993@gmail.com>
2020-05-08 20:16:56 -0300

    Automate Santa Fe update

--- csv_generator.py
+++ csv_generator.py
@@ -1,6 +1,7 @@
 from common import *
 from santa_fe_api import *
 from argentina_api import *
+import santa_fe_datosabiertos_joiner
 import os
 
 if __name__ == '__main__':
@@ -12,3 +13,5 @@ if __name__ == '__main__':
     # Generate Argentina csv's
     argentina_api = ArgentinaAPI('./')
     argentina_api.df_provinces.to_csv(os.path.join(CSV_FOLDER,'Argentina_Provinces.csv'))
+    # Download and join data from Santa Fe datos abiertos portal
+    santa_fe_datosabiertos_joiner.update_santa_fe()
