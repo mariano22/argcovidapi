3a3444c4576e7187ea00a7288f5a2a62bd21cbc1
Mariano Crosetti <marianocrosetti1993@gmail.com>
2020-05-08 20:08:01 -0300

    Update Santa Fe

--- santa_fe_datosabiertos_joiner.py
+++ santa_fe_datosabiertos_joiner.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import numpy as np
 import os
+from bs4 import BeautifulSoup, SoupStrainer
+import requests, zipfile, io
 
 import santa_fe_api
 from common import *
 
+SANTA_FE_DATOSABIERTOS_URL = "https://www.santafe.gob.ar/ms/covid19/datosabiertos/"
 DOWNLOAD_DIR = 'santa_fe_download/'
 LOCALIDAD_TO_DEPARTAMENTO_FILENAME = 'santa_fe_departamento_localidad.csv'
 NOTIFICACIONES_LOCALIDAD_FILENAME = 'notificaciones_localidad.csv'
@@ -21,8 +24,6 @@ def read_datosabiertos(last_date):
     df = df.rename(columns = {'localidad': 'PLACE',
                               'Confirmados': 'CONFIRMADOS',
                               'Descartados': 'DESCARTADOS',
-                              'Recuperados': 'RECUPERADOS',
-                              'Fallecidos': 'MUERTOS',
                               'En estudio': 'SOSPECHOSOS',
                               'Notificaciones': 'NOTIFICACIONES',
                                 })
@@ -32,7 +33,7 @@ def read_datosabiertos(last_date):
     df['DEPARTMENT'] = '#D_'+df['DEPARTMENT']
 
     # Shape with as API dataframe (TYPE, DEPARTMENT, PLACE as index, DATE as columns)
-    type_cols = ['CONFIRMADOS', 'DESCARTADOS', 'RECUPERADOS', 'MUERTOS', 'SOSPECHOSOS', 'NOTIFICACIONES']
+    type_cols = ['CONFIRMADOS', 'DESCARTADOS', 'SOSPECHOSOS', 'NOTIFICACIONES']
     df = pd.melt(df, id_vars=['DEPARTMENT','PLACE','Fecha'], value_vars=type_cols,var_name='TYPE', value_name='value')
     df['Fecha'] = df['Fecha'].apply(lambda date : pd.to_datetime(date,format='%Y-%m-%d'))
     df = df.pivot_table(index=['TYPE','DEPARTMENT','PLACE'], columns='Fecha', values='value')
@@ -47,7 +48,25 @@ def read_datosabiertos(last_date):
     df = santa_fe_api.complete_deparments_and_total(df)
     return df
 
-if __name__ == '__main__':
+
+def _santa_fe_zip_download():
+    page = requests.get(SANTA_FE_DATOSABIERTOS_URL)
+    data = page.text
+    soup = BeautifulSoup(data,features='lxml')
+
+    match_url = []
+    for link in soup.find_all('a'):
+        url = link.get('href')
+        if url.endswith('zip'):
+            match_url.append(url)
+    assert len(match_url)==1
+    zip_url = match_url[0]
+    r = requests.get(zip_url)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall(DOWNLOAD_DIR)
+
+def update_santa_fe():
+    _santa_fe_zip_download()
     # Read repo data
     df_api = santa_fe_api.SantaFeAPI('./', mock_drive=REPO_DATA_FILENAME).df
     # Read from Santa Fe datos abiertos
@@ -66,5 +85,8 @@ if __name__ == '__main__':
     assert all(df_new==df_result_restricted[df_result_restricted.columns[n_already_existing_columns:]])
 
     # Overwrite repo data
-    touch_timestamp()
     df_result.to_csv(REPO_DATA_FILENAME)
+
+if __name__ == '__main__':
+    update_santa_fe()
+    touch_timestamp()
